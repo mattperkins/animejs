cac6e50910b88a17a3b88c8d926b458d7ee75710
mattperkins <github@mattperkins.com>
2017-09-06 22:05:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## https://www.npmjs.com/package/react-anime
+## http://animejs.com/documentation/
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import Anime from 'react-anime'
+
+class App extends Component {
+  constructor() {
+    super();
+  this.state = {
+    name: 'Freddy'
+  }
+  }
+
+  render() {    
+    return (
+      <div>
+      <Anime easing="easeOutElastic"
+         duration={1000}
+         direction="alternate"
+         loop={true}
+         delay={(el, index) => index * 1000}
+         translateX='5rem'
+         scale={[.75, .9]}>
+            <div className="box"/>
+      </Anime>
+      <Anime easing="easeOutElastic"
+         duration={100}
+         direction="alternate"
+         loop={true}
+         delay={(el, index) => index * 4000}
+         translateX='3rem'
+         scale={[.75, .9]}>
+            <div className="box2"/>
+      </Anime>
+      </div>
+    );
+  }
+}
+
+
+render(<App />, document.getElementById('root'));
